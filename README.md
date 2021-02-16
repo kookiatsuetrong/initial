5598511d32f2dff0ee99524528ab00f1067ff0c1
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2021-02-17 01:17:12 +0700

    refactor cookie

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Initial project for Node.js web application
 - Nothing is the best.
 - Prefer simple code to complicated code.
 - Not everything I do is the best.
+- I've added some experiments, not sure they will cause some errors.
--- main.js
+++ main.js
@@ -11,18 +11,20 @@ var readCookie = cookie()
 var readBody   = express.urlencoded({extended:false})
 
 server.get ('/', showHome)
-server.get ('/member-register', readCookie, showRegisterPage)
-server.post('/member-register', readBody,   saveNewMember)
-server.get ('/member-login',    readCookie, showLogInPage)
-server.post('/member-login',    readBody,   checkPassword)
-server.get ('/member-profile',  readCookie, showProfile)
-server.get ('/member-logout',   readCookie, deleteSession)
+server.use (readCookie)
+server.use (extractCard)
+server.get ('/member-register', showRegisterPage)
+server.post('/member-register', readBody, saveNewMember)
+server.get ('/member-login',    showLogInPage)
+server.post('/member-login',    readBody, checkPassword)
+server.get ('/member-profile',  showProfile)
+server.get ('/member-logout',   deleteSession)
 
-server.get ('/post-add',        readCookie, showPostAdd)
-server.post('/post-add',        readCookie, readBody, savePost)
+server.get ('/post-add',        showPostAdd)
+server.post('/post-add',        readBody, savePost)
 server.get ('/post-detail',     showPostDetail)
 server.get ('/post-list',       showAllPost)
-// server.get ('/post-delete', readCookie, deletePost)
+// server.get ('/post-delete', deletePost)
 server.get ('/not-found',       showError)
 server.use (express.static('public'))
 server.use (showError)
@@ -31,27 +33,36 @@ function showHome(request, response) {
 	response.render('home.html')
 }
 
+function extractCard(request, response, next) {
+	request.card = request.cookies ? request.cookies.card : null
+	next()
+}
+
 function showRegisterPage(request, response) {
-	var card = request.cookies ? request.cookies.card : null
-	if (valid[card]) {
+	if (valid[request.card]) {
 		response.redirect('/member-profile')
 	} else {
 		response.render('member-register.html')
 	}
-}
+}	
 
 function saveNewMember(request, response) {
 	var e = request.body.email         || ''
 	var p = request.body.password      || ''
 	var f = request.body['first-name'] || ''
 	var g = request.body['last-name']  || ''
-	// TODO: Check data with regular expression
+	
+	e = isValidEmail(e)     ? e : ''
+	p = isValidPassword(p)  ? p : ''
+	f = isValidFirstName(f) ? f : ''
+	g = isValidLastName(g)  ? g : ''
+	
 	if (e == '' || p == '' || f == '' || g == '') {
 		response.redirect('/member-register')
 	} else {
-		var sql = "insert into member(email,password, " +
-				  "first_name,last_name)              " +
-				  "values(?,sha2(?,512),?,?)          "
+		var sql = " insert into member(email,password, " +
+				  " first_name,last_name)              " +
+				  " values(?,sha2(?,512),?,?)          "
 		var data = [e, p, f, g]
 		pool.query(sql, data, function show(error, resutl) {
 			if (error == null) {
@@ -65,8 +76,7 @@ function saveNewMember(request, response) {
 }
 
 function showLogInPage(request, response) {
-	var card = request.cookies ? request.cookies.card : null
-	if (valid[card]) {
+	if (valid[request.card]) {
 		response.redirect('/member-profile')
 	} else {
 		response.render('member-login.html')
@@ -79,8 +89,8 @@ function checkPassword(request, response) {
 	if (e == '' || p == '') {
 		response.redirect('/member-login')
 	} else {
-		var sql =   "select * from member where email = ? "
-					"and password = sha2(?, 512)          "
+		var sql =   " select * from member where email = ? "
+					" and password = sha2(?, 512)          "
 		var data = [e, p]
 		pool.query(sql, data, function show(error, result) {
 			if (result.length > 0) {
@@ -96,10 +106,9 @@ function checkPassword(request, response) {
 }
 
 function showProfile(request, response) {
-	var card = request.cookies ? request.cookies.card : null
-	if (valid[card]) {
+	if (valid[request.card]) {
 		var model = { }
-		model.member = valid[card]
+		model.member = valid[request.card]
 		response.render('member-profile.html', model)
 	} else {
 		response.redirect('/member-login')
@@ -107,8 +116,7 @@ function showProfile(request, response) {
 }
 
 function deleteSession(request, response) {
-	var card = request.cookies ? request.cookies.card : nulls
-	delete valid[card]
+	delete valid[request.card]
 	response.render('member-logout.html')
 }
 
@@ -131,8 +139,7 @@ function showError(request, response) {
 }
 
 function showPostAdd(request, response) {
-	var card = request.cookies ? request.cookies.card : null
-	if (valid[card]) {
+	if (valid[request.card]) {
 		response.render('post-add.html')
 	} else {
 		response.redirect('/member-login')
@@ -140,13 +147,12 @@ function showPostAdd(request, response) {
 }
 
 function savePost(request, response) {
-	var card = request.cookies ? request.cookies.card : null
-	if (valid[card]) {
-		var sql = "   insert into post(topic, detail, owner)  " +
+	if (valid[request.card]) {
+		var sql =   " insert into post(topic, detail, owner)  " +
 					" values(?, ?, ?)"
 		var t = request.body.topic  || ''
 		var d = request.body.detail || ''
-		var o = valid[card].code
+		var o = valid[request.card].code
 		if (t == '' || d == '') {
 			response.redirect('/post-add')
 		} else {
